f83f8e745616e575fad7238c8f9956725cd5fa80
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-08-23 15:03:37 -0700

    configure express session

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 // configures dotenv
 require('dotenv').config();
@@ -26,6 +27,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'WhatIsMyPurpose',
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
