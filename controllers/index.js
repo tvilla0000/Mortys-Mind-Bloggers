78c7aa8b859a063a4c73df0c5517ea3118b25852
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-08-23 18:14:32 -0700

    update

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+var user = require('../models/user');
+
+module.exports = {
+    index,
+
+}
+
+function index(req, res) {
+    user.find(function(err, users) {
+        res.render('index', {
+            users,
+            title: "Morty's Mind Bloggers",
+            user: req.user
+        })
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
+var indexCtrl = require('../controllers/index');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexCtrl.index);
+router.get('./auth/google', passport.authenticate(
+'google',
+{scope:  ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+))
+
+router.get('/logout', function(req, res) {
+  req.logOut();
+  res.redirect('/');
+})
 
 module.exports = router;
