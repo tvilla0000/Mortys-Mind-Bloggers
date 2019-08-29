30dfe2da92c9752608a09761236b3bc8c12e30e5
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-08-29 12:50:03 -0700

    add CRUD

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,15 +1,16 @@
-const comment = require('../models/comment');
-
+const Comment = require('../models/comment');
 
 module.exports = {
     index,
     create,
+    show,
+    update
 
 }
 
 function index(req, res) {
     res.render('comments', {
-        comment,
+        Comment,
         user: req.user,
         
         
@@ -19,5 +20,29 @@ function index(req, res) {
 
 
 function create(req, res) {
-    var newComment
+    // if (!req.user) res.status(401).send('must log in to access this!!');
+    var comment = new Comment(req.body);
+    comment.user = req.user.id;
+    comment.save(function(err) {
+        if(err) return next(err);
+        console.log(comment);
+        res.redirect(`/comments/${comment._id}`);
+    });
+}
+
+function show(req, res) {
+    Comment.findById(req.params.id, function(err, comment) {
+        res.render('showpage', {
+            comment,
+            user: req.user
+        });
+
+    })
+}
+
+function update(req, res) {
+    Comment.findByIdAndUpdate(id , req.body, function(err, comment) {
+        res.render('showpage', comment );
+
+    });
 }
