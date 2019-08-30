a08ccaa5479a408aeded5b50413e6915d4523cf6
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-08-29 23:40:13 -0700

    finish create function and export show function

--- controllers/episodes.js
+++ controllers/episodes.js
@@ -1,10 +1,17 @@
 const Episode = require('../models/episode');
 
 module.exports = {
-    create
+    create,
+    // show
 }
 
 function create(req, res) {
     if(!req.user) res.status(401).send("Must be logged in to execute this action!!");
-    
+    var episode = new episode(req.body);
+    episode.user = req.user.id;
+    episode.save(function(err) {
+        if(err) return next(err);
+        res.redirect(`/episodes/${episode._id}`);
+    })
 }
+
