975646d07e91038ad7f281116e77ecaec0d3a7db
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-08-30 08:59:37 -0700

    fixed application

--- routes/episodes.js
+++ routes/episodes.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const router = express.Router;
 const episodesCtrl = require('../controllers/episodes');
+const router = express.Router();
 
 router.post('/', episodesCtrl.create);
+router.get('/:id', episodesCtrl.show)
+
+module.exports = router;
